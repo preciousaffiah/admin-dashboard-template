22b742415d65969f2d62a8ccdb92df9efba130f0
preciousaffiah <preciousaffiah205@gmail.com>
2025-03-08 20:40:30 +0100

    working on landing oage

--- components/pages/start/start.tsx
+++ components/pages/start/start.tsx
@@ -14,34 +14,23 @@ const Start: FC = () => {
 
   return (
     <>
-      <Navbar/>
-        <Container className={"min-h-[40rem]"}>
-          <div className="authcard3 md:pt-24 md:pb-16 py-0 md:px-7 px-0">
-            <div className="authcard4">
-              <div className="md:w-1/2 w-full h-full z-50 rounded-xl md:bg-background bg-foreground">
-                <div className="text-txWhite gap-y-12 flex flex-col justify-center items-center h-full">
-                  <div className="text-center flex flex-col gap-y-5">
-                    <h2 className="font-semibold text-2xl">Welcome, {userData?.fullname}!</h2>
-                    <p className="">What would you like to do?</p>
-                  </div>
-                  <div className="flex flex-col gap-y-4">
-                    <Link
-                      href="#"
-                      className="font-medium rounded-[7px] justify-center items-center p-2 text-center w-full border-[0.3px] bg-primaryGreen text-black hover:bg-transparent border-primary-border hover:text-secondaryBorder flex gap-x-2 transition-colors duration-500"
-                    >
-                      This is the landing Page
-                    </Link>
-                   
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </Container>
-    {/* </GeneralLayout> */}
+      <Navbar />
+      <div className="pattern md:h-[40rem] h-[30rem] w-full px-4 cover">
+        <div className="m-auto h-full flex flex-col justify-center lg:text-8xl md:text-7xl sm:text-5xl text-4xl font-edu capitalize font-medium text-white">
+          <p className="lg:text-[7.5rem] md:text-[6.5rem] sm:text-[4.5rem] text-[3rem] uppercase">
+            Servlette
+          </p>
+          <p>
+            good
+            <span className="word">drink</span>
+            <span className="word">food</span>
+            <span className="word">life</span>
+          </p>
+        </div>
+      </div>
+      {/* </GeneralLayout> */}
     </>
   );
-
 };
 
 export default Start;
