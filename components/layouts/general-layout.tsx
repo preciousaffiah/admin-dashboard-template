25591a77c9903d69509fe157303fefd2d1af3b78
Precious Affiah <74688829+preciousaffiah@users.noreply.github.com>
2025-05-14 22:52:55 +0100

    Update general-layout.tsx

--- components/layouts/general-layout.tsx
+++ components/layouts/general-layout.tsx
@@ -48,26 +48,25 @@ export default function GeneralLayout({
         <meta property="og:site_name" content="Flenjo" />
         <meta property="og:locale" content="en_US" />
         <meta property="og:locale:alternate" content="en_US" />
-        <meta property="og:url" content="https://flenjo-admin.vercel.app/" />
+        <meta property="og:url" content="https://tryflenjo.com/" />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         <meta
           property="og:image"
-          content="https://res.cloudinary.com/dlq0uwrii/image/upload/v1728843002/Logo_qcsyol.png"
+                    content="https://storage.googleapis.com/flenjo-456113.appspot.com/random/circledFlenjo.png"
         />
         <meta property="twitter:card" content="summary_large_image" />
         <meta
           property="twitter:url"
-          content="https://flenjo-admin.vercel.app/"
+          content="https://tryflenjo.com/"
         />
         <meta property="twitter:title" content={title} />
         <meta property="twitter:description" content={description} />
         <meta
           property="twitter:image"
-          content="https://res.cloudinary.com/dlq0uwrii/image/upload/v1728843002/Logo_qcsyol.png"
+                    content="https://storage.googleapis.com/flenjo-456113.appspot.com/random/circledFlenjo.png"
         />
-        <link rel="icon" type="image/x-icon" href="https://res.cloudinary.com/dlq0uwrii/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1728843002/Logo_qcsyol.png" />
-      </Head>
+       <link rel="icon" type="image/x-icon" href="https://storage.googleapis.com/flenjo-456113.appspot.com/random/circledFlenjoIcon.png" />      </Head>
 
       <div className="flex flex-col h-screen min-h-screen">
         <PageAnimation>
